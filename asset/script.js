69a327ffda04a24587dd9a368962e9dc00f42fec
Safiyya H <safiyya.hasanova@gmail.com>
2021-05-23 12:08:30 -0500

    first commit

---
+++ asset/script.js
@@ -0,0 +1,29 @@
+// Assignment code here
+
+//Variables
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecial;
+var userChoices;
+//Assignin var
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var 
+//var upper
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
